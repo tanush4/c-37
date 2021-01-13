d9329d4ba7b8a7c1e20637f2f3eb805f0d5180fb
tanush4 <73825544+tanush4@users.noreply.github.com>
2021-01-13 17:15:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var database,gameState=0;
+var playerCount;
+var form,game,player;
+
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+game=new Game()
+game.getState()
+game.start()
+}
+
+function draw(){
+  background("white");
+  
+  
+    drawSprites();
+  
+}
